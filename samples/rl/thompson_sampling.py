2117fef45194dfb56cf0d8acd7717e732c389a89
Artemis <eminik35@hotmail.com>
2021-10-17 14:59:57 +0100

    Update bayesian bandit for normal rewards

--- samples/rl/thompson_sampling.py
+++ samples/rl/thompson_sampling.py
@@ -13,9 +13,13 @@ EXPECTED_REWARD_DISTRIBUTIONS = ('bernoulli', 'normal')
 
 
 class BayesianAgent(Agent):
-    def __init__(self, reward_distr='bernoulli'):
+    def __init__(self, reward_distr='bernoulli', known_reward_precision=None, mu_prior_mean=None, mu_prior_precision=None):
         if reward_distr not in EXPECTED_REWARD_DISTRIBUTIONS:
             raise ValueError('reward_distr must be in {}.'.format(EXPECTED_REWARD_DISTRIBUTIONS))
+
+        self.known_reward_precision = (known_reward_precision or 1) if reward_distr == 'normal' else None
+        self.mu_prior_mean = mu_prior_mean or 0  if reward_distr == 'normal' else None
+        self.mu_prior_precision = mu_prior_precision or 1  if reward_distr == 'normal' else None # 1 / variance
 
         self.reward_distr = reward_distr
         super().__init__()
@@ -27,19 +31,16 @@ class BayesianAgent(Agent):
         failures = bandit_record['actions'] - bandit_record['reward'] + 1
         return np.random.beta(a=successes, b=failures, size=1)[0]
 
-    @staticmethod
-    def _sample_normal(bandit_record):
-        reward_precision = 1 # 1 / variance
+    def _sample_normal(self, bandit_record):
+        reward_precision = self.known_reward_precision # 1 / variance
 
-        mu_prior_mean = 0
-        mu_prior_precision = 1
-
-        mu_posterior_precision = mu_prior_precision + reward_precision * bandit_record['actions']
+        mu_posterior_precision = self.mu_prior_precision + reward_precision * bandit_record['actions']
         mu_posterior_mean = (
-                mu_prior_mean * mu_prior_precision + reward_precision * bandit_record['reward']
+                self.mu_prior_mean * self.mu_prior_precision + reward_precision * bandit_record['reward']
         ) / mu_posterior_precision
+        mu_posterior_std = np.sqrt(1 / mu_posterior_precision)
 
-        return np.random.normal(loc=mu_posterior_mean, scale=np.sqrt(1 / reward_precision), size=1)
+        return np.random.normal(loc=mu_posterior_mean, scale=mu_posterior_std, size=1)[0]
 
     def _sample_bandit_mean(self, bandit):
         bandit_record = self.rewards_log[bandit]
@@ -59,18 +60,18 @@ class BayesianAgent(Agent):
 
             x = np.linspace(0, 1.0, 100)
             y = stats.beta.pdf(x, alpha, beta)
-            return x, y
         else:
-            reward_precision = 1
+            reward_precision = self.known_reward_precision
 
-            mu_prior_mean = 0
-            mu_prior_precision = 1  # 1 / variance
-
-            mu_posterior_precision = mu_prior_precision + reward_precision * bandit_record['actions']
-            mu_posterior_mean = (mu_prior_mean * mu_prior_precision + reward_precision * bandit_record[
+            mu_posterior_precision = self.mu_prior_precision + reward_precision * bandit_record['actions']
+            mu_posterior_mean = (self.mu_prior_mean * self.mu_prior_precision + reward_precision * bandit_record[
                 'reward']) / mu_posterior_precision
+            mu_posterior_std = np.sqrt(1 / mu_posterior_precision)
 
+            x = np.linspace(0, 20.0, 100)  # this should ideally be a variable
+            y = stats.norm.pdf(x, loc=mu_posterior_mean, scale=mu_posterior_std)
 
+        return x, y
 
     def take_action(self):
         samples = [self._sample_bandit_mean(bandit) for bandit in self.bandits]
